93ffcc5b935f367fb0dfd41ebe1c84c2ad514e2b
Simon Rasmussen <simon.d.rasmussen@gmail.com>
2020-10-08 20:33:30 +0200

    work in progess post views

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -14,6 +14,8 @@ class PostController extends Controller
      */
     public function index()
     {
+        $posts = Post::all();
+        dd($posts);
         return view('posts.index');
     }
 
