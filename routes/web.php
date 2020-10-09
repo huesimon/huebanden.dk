65ec03604184b6c5349c0cb678c8e2ee572694dd
Simon Rasmussen <simon.d.rasmussen@gmail.com>
2020-10-09 23:22:59 +0200

    Auth init

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,7 @@ Route::get('/', function () {
 Route::resources([
     'posts' => PostController::class,
 ]);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
