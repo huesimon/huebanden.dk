851527ca714eba469eefd03afe6d669f7da5fbda
Simon Rasmussen <simon.d.rasmussen@gmail.com>
2020-10-12 19:35:59 +0200

    Relationship for comments

--- database/migrations/2020_10_12_172624_create_comments_table.php
+++ database/migrations/2020_10_12_172624_create_comments_table.php
@@ -15,6 +15,9 @@ class CreateCommentsTable extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('post_id')->constrained();
+            $table->text('text');
             $table->timestamps();
         });
     }
