c6c512638d1c8b2a20700c4f5669b2c8a64801a4
Simon Rasmussen <simon.d.rasmussen@gmail.com>
2020-10-13 21:54:32 +0200

    Display a post and all its photos

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -12,9 +12,9 @@
         <!-- Project One -->
         <div class="row">
             <div class="col-md-7">
-                <a href="#">
-                    <img class="img-fluid rounded mb-3 mb-md-0" src="https://placehold.it/700x300" alt="">
-                </a>
+                @foreach ($post->photos as $photo)
+                    <img src="{{ asset($photo->path) }}">
+                @endforeach
             </div>
             <div class="col-md-5">
                 <h3>{{ $post->title }}</h3>
